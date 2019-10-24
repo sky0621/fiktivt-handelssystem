e09104ba903a5152bd5275cb2cf6a840301a4dc6
sky0621 <sky14work@gmail.com>
2019-10-25 01:12:35 +0900

    continue

---
+++ frontend/fiktivt-front/layouts/blog.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>ブログのナビゲーション</div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blog'
+}
+</script>
+
+<style scoped></style>
---
+++ frontend/fiktivt-front/pages/posts.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>ブログ１</h1>
+</template>
+
+<script>
+export default {
+  layout: 'blog',
+  name: 'Posts'
+}
+</script>
+
+<style scoped></style>
