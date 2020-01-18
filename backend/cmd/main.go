b0ab0b74624270566dbc062231fd657599172459
sky0621 <sky14work@gmail.com>
2020-01-18 12:28:58 +0900

    continue

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/sky0621/fiktivt-handelssystem/config"
 )
 
+// TODO: 既存である？
+type exitCode int
+
+const (
+	normalEnd   = 0
+	abnormalEnd = -1
+)
+
 func main() {
+	os.Exit(int(execMain()))
+}
+
+func execMain() exitCode {
 	cfg := config.Config{
 		RDBConfig: config.RDBConfig{
 			DBName:   os.Getenv("FIKTIVT_RDB_DBNAME"),
@@ -21,5 +36,19 @@ func main() {
 
 	app := di(cfg)
 	defer app.Shutdown()
-	app.Start()
+
+	// OSシグナル受信したらグレースフルシャットダウン
+	go func() {
+		q := make(chan os.Signal)
+		signal.Notify(q, os.Interrupt, os.Kill, syscall.SIGTERM)
+		<-q
+		app.Shutdown()
+		os.Exit(int(abnormalEnd))
+	}()
+
+	if err := app.Start(); err != nil {
+		fmt.Errorf("the application failed to start")
+		return abnormalEnd
+	}
+	return normalEnd
 }
